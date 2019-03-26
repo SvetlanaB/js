e0863ee95b31e23ee14235a3f58597beb09e10fd
Базарова Светлана <bazarova3001@gmail.com>
2019-03-27 01:20:12 +0300

    Перенесла код в script.js, добавила код с урока 1.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,52 @@
-alert ('Этот скрипт легче');
+//Урок 1 проба
+var numVar = 123; 
+let strVar = "123"; 
+let strVar2 = "Мама мыла";
+let undVar; 
+// const CONSTVAR = " раму"; 
+//const CONSTVAR = 2; 
+// const CONSTVAR = +"2";
+// const CONSTVAR = arrayVar;
+const CONSTVAR = "2";
+
+var boolVar = true; 
+var arrayVar = [1, 2, 3]; 
+var objectVar = {pole : 1, pole2 : 2}; 
+var nullVar = null; 
+
+console.log("Практика 1 урока")
+console.log (numVar + CONSTVAR);
+console.log(strVar + CONSTVAR);
+console.log(strVar2 + CONSTVAR);
+console.log(undVar + CONSTVAR);
+console.log(boolVar + CONSTVAR);
+console.log(arrayVar + CONSTVAR);
+console.log(objectVar + CONSTVAR);
+console.log(nullVar + CONSTVAR);
+
+		
+/*var num = 1000;
+var str = "108";
+
+console.log(num + str);*/
+
+
+// ДЗ 1 урок
+var tC = 30;
+var tF = (9 / 5) * tC + 32;
+
+console.log("ДЗ 1");
+console.log("Температура по Фаренгейту:");
+console.log(tF);
+
+
+var name = "Василий";
+var admin = name;
+
+console.log("Работа с переменными:");
+console.log(admin);
+
+
+
+//alert ('Этот скрипт легче'); //это для async/defer
+
